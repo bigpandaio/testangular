9af574ad7f6ae98c24f7c51e82c85205c090f723
Shahar Kedar <shahar@bigpanda.io>
2013-08-10 11:07:31 +0300

    First implementation of the Login Service using promises

--- app/js/services/login.service.js
+++ app/js/services/login.service.js
@@ -1,7 +1,20 @@
-app.service('LoginService', ['$http', function($http) {
+app.service('LoginService', ['$http', '$q', function ($http, $q) {
 
-  this.login = function(username, password) {
-    //TODO: implement
+  this.login = function (username, password) {
+    var deferred = $q.defer();
+
+    var futureResponse = $http.post('http://localhost:3000/login', {username: username, password: password});
+    futureResponse.then(function () {
+      deferred.resolve({});
+    }, function (response) {
+      if (response.status == 401) {
+        deferred.reject("Invalid username or password");
+      } else {
+        deferred.reject(response.data);
+      }
+    })
+
+    return deferred.promise;
   }
 
 }])
