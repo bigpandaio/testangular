c3e4488febab6aa5af2a85b6fe73ebaa9bc71727
Shmeff <shmeff@bigpanda.io>
2013-08-10 11:23:03 +0300

    Controller logic - Still work in progress

--- app/js/controllers/login.controller.js
+++ app/js/controllers/login.controller.js
@@ -1,7 +1,51 @@
 app.controller('LoginController', ['$scope', function($scope) {
 
+  // Step: 1 Just the controller
+
+  // Step 4: Add the properties.
+  $scope.username = null;
+  $scope.password = null;
+  $scope.valid = false;
+
+
+  // Step 5: Add the function without functionallity
+
+//  $scope.login = function() {
+//  }
+
+  // Step 7: Add basic login check for values
   $scope.login = function() {
-    alert("bla");
-  }
+    if (!$scope.valid) {
+      $scope.error = "Please fill in all fields";
+    }
+  };
+
+  // Step 11: Add LoginService
+  // app.controller('LoginController', ['$scope', 'loginService', function($scope, loginService) {
+  // Step 12: Add logic for calling service
+
+//  $scope.login = function() {
+//    if (!$scope.valid) {
+//      $scope.error = "Please fill in all fields";
+//
+//    } else {
+//      // TODO - submitting class ?
+//
+//      loginService.login($scope.username, $scope.password);
+//    }
+//  };
+
+  // Step 14: Add full logic of using the promise
+
+//  loginService.login($scope.username, $scope.password).then(
+//      function() {
+//        // broadcast login ??
+//      },
+//      function() {
+//        // set error to invalid user name / password
+//      }
+  );
+
+
 
 }])
--- test/unit/controllers/login.contorller.spec.js
+++ test/unit/controllers/login.contorller.spec.js
@@ -1,24 +1,82 @@
 describe('Login controller tests', function () {
 
+  // TODO - Move controller creation to the beforeEach ?
+
+
+  // Steps:
+
+  // Step 1: Just the describe line
+
+  // Step 2: Add before each with module, inject expect
+
   var inject = angular.mock.inject;
   var module = angular.mock.module;
   var expect = chai.expect;
 
+
   beforeEach(function () {
-    module('testangular', function($provide) {
-      $provide.value('LoginService', {});
-    });
+    module('testangular');
   });
 
-  describe('#login', function() {
 
-    it('should call LoginService#login', inject(['$controller', '$rootScope', function($controller, $rootScope) {
+  // Step 3: Scope creation - Add a simple test with controller creation
+
+  describe('Initialize Scope', function() {
+
+    it('Should set initial properties', inject(function($controller, $rootScope) {
       var scope = $rootScope.$new();
       $controller("LoginController", { $scope: scope });
 
-      expect(1).to.eql(1);
-    }]))
-  })
+      expect(scope).to.have.property("username", null);
+      expect(scope).to.have.property("password", null);
+    }));
+
+    it('Should start as invalid', inject(function($controller, $rootScope) {
+      var scope = $rootScope.$new();
+      $controller("LoginController", { $scope: scope });
+
+      expect(scope).to.have.property("valid", false);
+    }));
+  });
+
+
+  // Step 6: Adding functionallity to the login method
+
+  describe('Invalid input', function() {
+
+    it('Should add error when no input', inject(function($controller, $rootScope) {
+      var scope = $rootScope.$new();
+      $controller("LoginController", { $scope: scope });
+
+      scope.login();
+
+      expect(scope.error).to.be.eq("Please fill in all fields");
+    }));
+
+
+    // Step 8: Add validation check using the directive
+
+    it('Should add error when data is invalid', inject(function($controller, $rootScope) {
+      var scope = $rootScope.$new();
+      $controller("LoginController", { $scope: scope });
+
+      scope.username = "test";
+
+      scope.login();
+
+      expect(scope.error).to.be.eq("Please fill in all fields");
+    }));
+  });
+
+  // Step 9: Add a check login service wasn't called
+  // Require adding mock for LoginService
+
+  // Step 10: Adding test for valid input
+  // LoginService is called
+
+  // Step 13: Add a check for valid login and invalid login (using mock)
+
+
 
 
 });
