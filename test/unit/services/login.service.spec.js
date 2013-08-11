c6de130fc48cb1728e9c5549c39ea662f8e43498
Shahar Kedar <shahar@bigpanda.io>
2013-08-11 09:34:00 +0300

    Minimum viable code for the presentation

--- test/unit/services/login.service.spec.js
+++ test/unit/services/login.service.spec.js
@@ -1,42 +1,78 @@
 describe('Login controller tests', function () {
 
-  var inject = angular.mock.inject;
-  var module = angular.mock.module;
   var expect = chai.expect;
 
   // STEP 1: Initialize module before each test case
   beforeEach(function () {
-    module('testangular', function($provide) {});
+    module('testangular', function ($provide) {
+    });
   });
 
   // TODO: STEP X: It is considered best practice to verify, after each test case, that
   // there are no additional outstanding expectations or requests
-  afterEach(function() {
-    inject(['$httpBackend', function($http) {
+  afterEach(function () {
+    inject(['$httpBackend', function ($http) {
       $http.verifyNoOutstandingExpectation();
       $http.verifyNoOutstandingRequest();
     }]);
   })
 
-  describe('#login', function() {
+  describe('#login', function () {
 
-    // STEP 2: We can configure expectations on Angular's $httpBackend mock object.
-    // You can capture requests that are expected to be sent over HTTP to a certain endpoint.
+    // STEP 2: We can configure Request Expectations on Angular's $httpBackend mock object.
+    // In this test case we make sure that calling #login on the service results in an HTTP POST request to /login.
+    // Notice that the test will fail in case the expected HTTP call is not executed.
     it('should send an http POST request to http://<domain>/login',
-        inject(['$httpBackend', 'LoginService', function($http, loginService) {
 
-          // You can use regular expression to capture all requests to a certain endpoint regardless
+        // Use angular's built-in injection mechanism to get a hold of the mock $httpBackend object
+        inject(['$httpBackend', 'LoginService', function ($httpBackend, loginService) {
+
+          // You can capture requests that are expected to be sent over HTTP to a certain endpoint.
+          // Use regular expression to capture all requests to a certain endpoint regardless
           // of the domain to which they are sent. This provides more flexibility when testing your app
           // with different configuration per environment.
-          $http.expectPOST(/http:\/\/.+\/login/).respond(200);
-          loginService.login("shahar@acme.com", "testangular");
+          $httpBackend.expectPOST(/http:\/\/.+\/login/).respond(200);
+          loginService.login("wileecayote@acme.com", "testangular");
 
           // You must explicitly call $httpBackend#flush() to execute the mock behavior you defined.
           // Explicitly flushing requests turns the interaction with the $http service to be synchronous
           // while preserving the async api of the backend.
-          $http.flush();
+          $httpBackend.flush();
+        }]))
 
-    }]))
+    // STEP 3: Alternitively, we can configure Backend Definitions on Angular's $httpBackend mock object.
+    // In contrast to Request Expectations, Backend Definitions only define how the $httpBackend should
+    // behave under certain conditions. The test might pass whether the expected HTTP call was executed or not.
+    it('should handle HTTP status code 401',
+
+        inject(['$httpBackend', 'LoginService', function ($httpBackend, loginService) {
+
+          // Use the whenX methods to configure Backend Definitions
+          $httpBackend.whenPOST(/http:\/\/.+\/login/).respond(401);
+          loginService.login("wileecayote@acme.com", "testangular", function (error) {
+            expect(error).to.equal('Invalid username or password');
+          });
+
+          $httpBackend.flush();
+
+        }]));
+
+    // This is the exact same test case as above only using the Promise paradigm instead of plain old callbacks.
+    // Look how nice the code looks!
+//    it('should handle HTTP status code 401', function() {
+//
+//      inject(['$httpBackend', 'LoginService', function($httpBackend, loginService) {
+//
+//        $httpBackend.whenPOST(/http:\/\/.+\/login/).respond(401);
+//        var promise = loginService.login("wileecayote@acme.com", "testangular");
+//
+//        $httpBackend.flush();
+//
+//        expect(promise).to.fail.with('Invalid username or password');
+//
+//
+//      }]);
+//    })
   })
 
 
