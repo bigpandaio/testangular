13fd2bb9e22d191256e5e7c32e482de254b25890
Shahar Kedar <shahar@bigpanda.io>
2013-08-12 10:51:35 +0300

    connect task is redundant

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,16 +31,6 @@ module.exports = function (grunt) {
       }
     },
 
-    connect: {
-      server: {
-        options: {
-          port: 8000,
-          base: 'public',
-          keepalive: true
-        }
-      }
-    },
-
     karma: {
       options: {
         configFile: 'karma.conf.js'
