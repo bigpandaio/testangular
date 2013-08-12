495d56c0cdb19ddb475834a3885c31e39e52c566
Shmeff <shmeff@bigpanda.io>
2013-08-12 10:23:40 +0300

    Adding skeleton for the e2e tests

---
+++ test/e2e/login.form.e2e.spec.js
@@ -0,0 +1,9 @@
+describe('End to end test for the login form', function () {
+
+  // TODO - add the two tests.
+
+  it("Should show error if invalid email");
+
+  it("Should show be successful if valid username + password");
+  
+});
