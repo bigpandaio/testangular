ba01f587fc24e1eb1071385463ac06745f94042b
Shahar Kedar <shahar@bigpanda.io>
2013-08-12 10:52:18 +0300

    karma-e2e configuration

---
+++ karma-e2e.conf.js
@@ -0,0 +1,71 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['ng-scenario'],
+    files: [
+      'test/e2e/**/*.js'
+    ],
+
+    // use dots reporter, as travis terminal does not support escaping sequences
+    // possible values: 'dots', 'progress'
+    // CLI --reporters progress
+    reporters: ['dots'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    // CLI --colors --no-colors
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    // CLI --log-level debug
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    // CLI --auto-watch --no-auto-watch
+    autoWatch: false,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    // CLI --browsers Chrome,Firefox,Safari
+    browsers: ['Chrome'],
+
+    proxies: {
+      '/': 'http://localhost:8000/'
+    },
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    // CLI --capture-timeout 5000
+    captureTimeout: 20000,
+
+    // Auto run tests on start (when browsers are captured) and exit
+    // CLI --single-run --no-single-run
+    singleRun: false,
+
+    // report which specs are slower than 500ms
+    // CLI --report-slower-than 500
+    reportSlowerThan: 500,
+
+    plugins: [
+      // Launchers
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+
+      //Framework
+      'karma-ng-scenario'
+    ]
+
+
+  })
+}
+
+
