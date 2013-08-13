a9678cd364b0bc59f3e7c1caef02c626d2365222
Shahar Kedar <shahar@bigpanda.io>
2013-08-13 16:00:34 +0300

    Organizing our notes on the service and service spec

--- app/js/services/login.service.js
+++ app/js/services/login.service.js
@@ -3,12 +3,18 @@ app.service('LoginService', ['$http', '$q', function ($http, $q) {
   this.login = function (username, password, callback) {
     var deferred = $q.defer();
 
+    // Step: how we use $http to post a request to our server
     var futureResponse = $http.post('http://localhost:8000/login', {username: username, password: password});
+
+
     futureResponse.then(function () {
       deferred.resolve({});
     }, function (response) {
       if (response.status == 401) {
+
+        // Step: in case of HTTP 401, we return an error
         deferred.reject(new Error("Invalid username or password"));
+
       } else {
         deferred.reject(response.data);
       }
