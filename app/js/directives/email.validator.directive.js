22cbda2ae79279a916fc3193d1f80f20fd6d2f4f
Shmeff <shmeff@bigpanda.io>
2013-08-14 10:16:53 +0300

    Directive updates to be only on email

---
+++ app/js/directives/email.validator.directive.js
@@ -0,0 +1,31 @@
+// Step: Start with the directive decleration
+
+app.directive('emailValidator', function() {
+
+  // Step: The validation function
+  function validateEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  return {
+    restrict:'A',
+
+    // Step: Create the link function
+    link: function(scope, element) {
+
+      scope.valid = false;
+
+      // Step: Bind the element for checking username content
+
+      element.bind('input', function() {
+
+        var value = element.val();
+
+        // Step: Run the change inside an $apply loop
+        scope.$apply(function() {
+          scope.valid = validateEmail(value);
+        })
+      });
+    }
+  }
+});
