5ece8c06395ceda13f546e863102c67fd43c41f2
Shahar Kedar <shahar@bigpanda.io>
2013-08-14 10:21:20 +0300

    Fixed mocha runner

--- plain-js/utils.spec.js
+++
@@ -1,7 +0,0 @@
-var expect = chai.expect;
-
-describe('Test our plain old JavaScript function', function() {
-  it('should invalidate email without domain', function() {
-    expect(validateEmail('wileecayote@')).to.be.false;
-  })
-})
