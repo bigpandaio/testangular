a509aee388d8e8af9773fb7e1f533e938e2ea19a
Shmeff <shmeff@bigpanda.io>
2013-08-14 10:18:31 +0300

    Adding comments for the plainJS test

--- plain-js/test/utils.spec.js
+++ plain-js/test/utils.spec.js
@@ -1,7 +1,11 @@
+// Step: Define chai
 var expect = chai.expect;
 
+// Step: Define the test
 describe('Test our plain old JavaScript function', function() {
   it('should invalidate email without domain', function() {
+
+    // Step: add the expectation
     expect(validateEmail('wileecayote@')).to.be.false;
   })
 })
