3ac9f4045f0da6ee56cde72ffc3ddcff910caa5b
Shahar Kedar <shahar@bigpanda.io>
2013-08-14 10:45:33 +0300

    Better description

--- plain-js/test/utils.spec.js
+++ plain-js/test/utils.spec.js
@@ -3,7 +3,7 @@ var expect = chai.expect;
 
 // Step: Define the test
 describe('Test our plain old JavaScript function', function() {
-  it('should invalidate email without domain', function() {
+  it('should reject email without domain', function() {
 
     // Step: add the expectation
     expect(validateEmail('wileecayote@')).to.be.false;
